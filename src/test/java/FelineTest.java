17ce25bcdae87f3e1b3827588d76da7cbd763658
Artemii Petrov <deidara941@yandex.ru>
2025-05-13 21:02:36 +0300

    Added tests for Feline, Lion, Cat

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,49 +1,32 @@
 import com.example.Feline;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import static com.example.StaticVariables.FELINE_FAMILY;
 import static com.example.StaticVariables.LIST_FOOD_FOR_FELINE;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class FelineTest {
-    private Feline feline;
+    private final Feline feline = new Feline();
 
-    @Mock
-    private Feline felineMock;
-    @Spy
-    private Feline felineSpy;
-
-    @Before
-    public void createFeline() {
-        felineMock = new Feline();
-    }
 
     @Test
     public void eatMeatTest() throws Exception {
-        Assert.assertEquals("Неверный список продуктов", LIST_FOOD_FOR_FELINE, felineMock.eatMeat());
+        Assert.assertEquals("Неверный список продуктов", LIST_FOOD_FOR_FELINE, feline.eatMeat());
     }
 
     @Test
     public void getFamilyTest() {
-        Assert.assertEquals("Неверный список продуктов", FELINE_FAMILY, felineMock.getFamily());
+        Assert.assertEquals("Неверное семейство", FELINE_FAMILY, feline.getFamily());
     }
 
     @Test
-    public void getKittensWithParamEquals1Test() {
-        felineSpy.getKittens(1);
-        Assert.assertEquals(1, felineSpy.getKittens());
+    public void getKittensWithParamsTest() {
+        Assert.assertEquals("Неверное количество", 5, feline.getKittens(5));
     }
 
     @Test
     public void getKittensWithoutParamsTest() {
-        felineSpy.getKittens();
-        Mockito.verify(felineSpy, Mockito.times(1)).getKittens();
+        Assert.assertEquals("Неверное количество", 1, feline.getKittens());
     }
 }
